8fa912286b0d878f3e60cc2ef87f0daec7989511
SmartNibas <smartnibas@gmail.com>
2026-05-23 16:28:33 +0530

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,11 +4,11 @@ import GoogleProvider from "next-auth/providers/google";
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.926951295295-obdv8sq25j0bs61kdoveulu6tindk21c.apps.googleusercontent.com!,
-      clientSecret: process.env.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!,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  secret: process.env.6a9cb61f756b7e66512ce3f37c1048ad,
+  secret: process.env.NEXTAUTH_SECRET,
 });
 
 export { handler as GET, handler as POST };
